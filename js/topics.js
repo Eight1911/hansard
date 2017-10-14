0e8998437b408d3a7ca317e3d799d9d8975bc125
eight1911 <15707729+Eight1911@users.noreply.github.com>
2017-10-14 15:29:21 -0400

    fixed bugs

--- js/topics.js
+++ js/topics.js
@@ -1,5 +1,5 @@
 
-function makegraph(item, years) {
+function makegraph(item) {
   let height = 3,
       width  = 10,
       colwidth = width / 104, // there are 104 years
@@ -19,12 +19,14 @@ function makegraph(item, years) {
     .attr("y", (d) => (height - height*d) + "em")
     .attr("fill", "#aaa")
 
-
 }
 
 // add a topic line to a div with data already entered
-function makelines(table, keys, years) {
-  let percent = (i) => Math.round(i*100000)/1000 + "%"
+function makelines(table, keys) {
+  console.log(d3.max(keys.map((i) => i[1])))
+  let maxprop = d3.max(keys.map((i) => i[1])),
+      percent = (i) => Math.round(i*100000)/1000 + "%"
+
   let lines = table
     .data(keys)
     .enter()
@@ -34,16 +36,31 @@ function makelines(table, keys, years) {
     .append("td")
     .text((i) => i[0])
 
-  makegraph(lines.append("td"), years)
+  makegraph(lines.append("td"))
 
   lines
     .append("td")
     .text((i) => i[2].join(" "))
 
+  let svg = lines
+    .append("td")
+    .style("width", "15vw")
+    .append("svg")
+    .style("width", "15vw")
+    .style("height", "1em")
+
+  svg
+    .append("rect")
+    .attr("height", "1em")
+    .attr("width", (d) => (15*d[1]/maxprop) + "vw")
+    .attr("x", (d) => (15 - 15*d[1]/maxprop) + "vw")
+    .attr("fill", "#aaa")
+
   lines
     .append("td")
     .text((i) => percent(i[1]))
-
+    .style("width", "5vw")
+    .style("text-align", "left")
 
 }
 
@@ -71,10 +88,11 @@ function maketable() {
     .text((d) => d["text"])
     .attr("class", (d) => d["propotion"])
     .style("width", (d) => d["width"])
+    .attr("colspan", (d) => d["text"] == "proportion" ? "2" : "1")
 
   return table
-
 }
+
 
 function callback(error, keys) {
   data['coll_keys'] = keys
@@ -83,7 +101,15 @@ function callback(error, keys) {
   let table = maketable()
       lines = makelines(table, keys)
 
-  d3.select("section.body").style("background", "none")
+  d3.select("section.background")
+    .style("transition", "2s")
+    .style("opacity", "0")
+
+  d3.select("div.wrapper")
+    .transition()
+    .delay(1000)
+    .style("transition", "2s")
+    .style("opacity", "2")
 }
 
 d3.queue()
