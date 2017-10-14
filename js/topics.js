c8d8203e4922ebc0be055a203e5e4578821608a6
eight1911 <15707729+Eight1911@users.noreply.github.com>
2017-10-14 17:35:04 -0400

    fixed bugs

--- js/topics.js
+++ js/topics.js
@@ -1,116 +1,237 @@
 
-function makegraph(item) {
-  let height = 3,
-      width  = 10,
-      colwidth = width / 104, // there are 104 years
-      svg = item
-    .append("svg")
-    .style("width", width +"vw")
-    .style("height", height + "em")
-    .selectAll("rect")
-    .data((d) => d[3])
-    .enter()
 
-  svg
-    .append("rect")
-    .attr("width", colwidth+ "em") // width / number of columns
-    .attr("height", (d) => height*d + "em")
-    .attr("x", (d, i) => i*colwidth + "vw")
-    .attr("y", (d) => (height - height*d) + "em")
-    .attr("fill", "#aaa")
+function compute_mean(keys) {
+
+  function expectation(array) {
+    let norm = d3.sum(array)
+    let summ = 0
+    for (var i = array.length; i--;) 
+      summ += i * array[i]
+    return summ / (array.length * norm)
+  }
+
+  return keys.map((d) => expectation(d[3]))
 
 }
 
-// add a topic line to a div with data already entered
-function makelines(table, keys) {
-  console.log(d3.max(keys.map((i) => i[1])))
-  let maxprop = d3.max(keys.map((i) => i[1])),
-      percent = (i) => Math.round(i*100000)/1000 + "%"
+function initialize(keys) {
 
-  let lines = table
-    .data(keys)
-    .enter()
-    .append("tr")
 
-  lines
-    .append("td")
-    .text((i) => i[0])
+  function makegraph(item) {
+    let td = item
+      .append("td")
+      .attr("class", "overtime")
 
-  makegraph(lines.append("td"))
+    let height = 3,
+        width  = 10,
+        colwidth = width / 104, // there are 104 years
+        svg = td
+      .append("svg")
+      .attr("class", "overtime")
+      .style("width", width +"vw")
+      .style("height", height + "em")
+      .style("text-align", "center")
+      .selectAll("rect")
+      .data((d) => d[3])
+      .enter()
 
-  lines
-    .append("td")
-    .text((i) => i[2].join(" "))
+    svg
+      .append("rect")
+      .attr("width", colwidth + "em") // width / number of columns
+      .attr("height", (d) => height*d + "em")
+      .attr("x", (d, i) => i*colwidth + "vw")
+      .attr("y", (d) => (height - height*d) + "em")
+      .attr("fill", "#aaa")
 
-  let svg = lines
-    .append("td")
-    .style("width", "15vw")
-    .append("svg")
-    .style("width", "15vw")
-    .style("height", "1em")
+  }
 
-  svg
-    .append("rect")
-    .attr("height", "1em")
-    .attr("width", (d) => (15*d[1]/maxprop) + "vw")
-    .attr("x", (d) => (15 - 15*d[1]/maxprop) + "vw")
-    .attr("fill", "#aaa")
 
-  lines
-    .append("td")
-    .text((i) => percent(i[1]))
-    .style("width", "5vw")
-    .style("text-align", "left")
+  // add a topic line to a div with data already entered
+  function makelines(table, keys) {
+    let maxprop = d3.max(keys.map((i) => i[1])),
+        percent = (i) => Math.round(i*100000)/1000 + "%"
+
+    let lines = table
+      .data(keys)
+      .enter()
+      .append("tr")
+
+    lines
+      .append("td")
+      .attr("class", "id")
+      .style("text-align", "center")
+      .text((i) => i[0])
+
+    makegraph(lines)
+
+    lines
+      .append("td")
+      .attr("class", "words")
+      .style("text-align", "left")
+      .text((i) => i[2].join(" "))
+
+    let svg = lines
+      .append("td")
+      .attr("class", "freqblock")
+      .style("text-align", "right")
+      .style("width", "15vw")
+      .append("svg")
+      .attr("class", "freqblock")
+      .style("width", "15vw")
+      .style("height", "1em")
+      .append("rect")
+      .style("text-align", "left")
+      .attr("height", "1em")
+      .attr("width", (d) => (15*d[1]/maxprop) + "vw")
+      .attr("x", (d) => (15 - 15*d[1]/maxprop) + "vw")
+      .attr("fill", "#aaa")
+
+    lines
+      .append("td")
+      .attr("class", "percentage")
+      .text((i) => percent(i[1]))
+      .style("width", "5vw")
+      .style("text-align", "left")
+
+  }
+
+  function maketable() {
+    let data  = [
+      {"text": "ID", "width": "4vw" },
+      {"text": "over time", "width": "11vw"},
+      {"text": "base words", "width": "60vw"},
+      {"text": "proportion", "width": "20vw"},
+    ]
+
+    let table = d3
+      .select("div.wrapper")
+      .append("table")
+      .attr("class", "topic-content")
+      .selectAll("div")
+
+    header = table
+      .append('tr')
+      .data(data)
+      .enter()
+      .append("th")
+      .text((d) => d["text"])
+      .attr("class", (d) => d["propotion"])
+      .style("width", (d) => d["width"])
+      .attr("colspan", (d) => d["text"] == "proportion" ? "2" : "1")
+
+    return table
+  }
+
+  function main(keys) {
+    let table = maketable()
+        lines = makelines(table, keys)
+  }
+
+  return main(keys)
 
 }
 
-function maketable() {
-  let data  = [
-    {"text": "ID", "width": "4vw" },
-    {"text": "over time", "width": "11vw"},
-    {"text": "base words", "width": "60vw"},
-    {"text": "proportion", "width": "20vw"},
-  ]
+function update(keys) {
 
-  let table = d3
-    .select("div.wrapper")
-    .selectAll("table")
-    .append("table")
+  function makegraph(item) {
+    let svg = item
+      .select("td.overtime")
+      .select("svg.overtime")
 
+    let height = 3,
+        width  = 10,
+        colwidth = width / 104 // there are 104 years
 
-  header = table
-    .append('tr')
+    svg
+      .selectAll("rect")
+      .data((d) => d[3])
+      .attr("height", (d) => height*d + "em")
+      .attr("y", (d) => (height - height*d) + "em")
 
-  header
-    .data(data)
-    .enter()
-    .append("th")
-    .text((d) => d["text"])
-    .attr("class", (d) => d["propotion"])
-    .style("width", (d) => d["width"])
-    .attr("colspan", (d) => d["text"] == "proportion" ? "2" : "1")
+  }
 
-  return table
+  function updatelines(table, keys) {
+    let maxprob = d3.max(keys.map((i) => i[1])),
+        percent = (i) => Math.round(i*100000)/1000 + "%"
+
+    let lines = table
+      .selectAll("tr")
+      .data(keys)
+
+    lines
+      .select("td.id")
+      .text((i) => i[0])
+
+    makegraph(lines)
+
+    lines
+      .select("td.words")
+      .text((i) => i[2].join(" "))
+
+    let svg = lines
+      .select("td.freqblock")
+      .select("svg.freqblock")
+      .select("rect")
+      .transition()
+      .duration(1500)
+      .attr("width", (d) => (15*d[1]/maxprob) + "vw")
+      .attr("x", (d) => (15 - 15*d[1]/maxprob) + "vw")
+
+    lines
+      .select("td.percentage")
+      .text((i) => percent(i[1]))
+
+  }
+
+  function main(keys) {
+    let table = d3
+      .select("table.topic-content")
+    let lines = updatelines(table, keys)
+  }
+
+  return main(keys)
+
 }
 
+
+function sort(name, ascending) {
+  let means = data['means']
+  let comparison = ascending ? {
+    "ID":         (a, b) => a[0] - b[0],
+    "over time":  (a, b) => means[a[0]] - means[b[0]],
+    "base words": (a, b) => a[2][0].localeCompare(b[2][0]),
+    "proportion": (a, b) => a[1] - b[1]
+  } : {
+    "ID":         (a, b) => - (a[0] - b[0]),
+    "over time":  (a, b) => - (means[a[0]] - means[b[0]]),
+    "base words": (a, b) => - a[2][0].localeCompare(b[2][0]),
+    "proportion": (a, b) => - (a[1] - b[1])
+  }
+
+  return update(data['coll_keys'].sort(comparison[name]))
+
+
+}
 
 function callback(error, keys) {
   data['coll_keys'] = keys
+  data['means'] = compute_mean(keys)
   //data['coll_years'] = years
-  
-  let table = maketable()
-      lines = makelines(table, keys)
+
+  initialize(keys)
 
   d3.select("section.background")
-    .style("transition", "2s")
+    .style("transition", "1.5s")
     .style("opacity", "0")
 
   d3.select("div.wrapper")
     .transition()
     .delay(1000)
-    .style("transition", "2s")
-    .style("opacity", "2")
+    .style("transition", "1.5s")
+    .style("opacity", "1")
 }
+
+
 
 d3.queue()
   .defer(d3.json, "https://eight1911.github.io/page/data/coll/keys.json")
