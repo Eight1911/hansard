2256df74b1772fbaeb251a799b2d354b99330f1e
eight1911 <15707729+Eight1911@users.noreply.github.com>
2017-10-16 03:27:25 -0400

    optimized sort and added search

--- es6/topics.js
+++ es6/topics.js
@@ -1,18 +1,5 @@
 (() => {
 
-  function array_comp(a, b) {
-
-    let min = Math.min(a.length, b.length)
-    for (let i = 0; i < min; ++i) {
-      if (a[i] > b[i])
-        return 1
-      else if (a[i] < b[i])
-        return -1
-    }
-    return a.length - b.length
-  }
-
-
   function compute_mean(keys) {
 
     function expectation(array) {
@@ -23,7 +10,7 @@
       return summ / (array.length * norm)
     }
 
-    return keys.map(d => expectation(d[3]))
+    return keys.forEach(d => d.push(expectation(d[3])))
   }
 
 
@@ -32,247 +19,142 @@
     function makegraph(item) {
       let td = item
         .append("td")
-        .attr("class", "overtime")
 
       let height = 3,
           width  = 10,
           colwidth = width / 104, // there are 104 years
           svg = td
         .append("svg")
-        .attr("class", "overtime")
-        .style("width", width +"vw")
-        .style("height", height + "em")
-        .style("text-align", "center")
+          .attr("class", "topic-graph")
+          .style("width", width +"vw")
+          .style("height", height + "em")
+          .style("text-align", "center")
         .selectAll("rect")
         .data(d => d[3])
         .enter()
 
       svg
         .append("rect")
-        .attr("width", colwidth + "em") // width / number of columns
-        .attr("height", d => height*d + "em")
-        .attr("x", (d, i) => i*colwidth + "vw")
-        .attr("y", d => (height - height*d) + "em")
-        .attr("fill", "#aaa")
+          .attr("width", colwidth + "em") // width / number of columns
+          .attr("height", d => height*d + "em")
+          .attr("x", (d, i) => i*colwidth + "vw")
+          .attr("y", d => (height - height*d) + "em")
+          .attr("fill", "#aaa")
     }
 
     // add a topic line to a div with data already entered
-    function makelines(table, keys) {
+    function makelines(tbody, keys) {
       let maxprop = d3.max(keys.map((i) => i[1])),
           percent = (i) => Math.round(i*100000)/1000 + "%"
 
-      let lines = table
+      let lines = tbody
+        .selectAll("tr")
         .data(keys)
         .enter()
         .append("tr")
 
       lines
         .append("td")
-        .attr("class", "id")
-        .style("text-align", "center")
-        .text((i) => i[0])
+          .attr("class", "topic-id")
+          .style("text-align", "center")
+          .text(d => d[0])
 
       makegraph(lines)
+      // this is for sorting with list.js only
+      lines
+        .append("td")
+          .attr("class", "topic-overtime")
+          .text(d => percent(d[4]))
+          .style("display", "none")
+
 
       lines
         .append("td")
-        .attr("class", "words")
-        .style("text-align", "left")
-        .text((i) => i[2].join(" "))
+          .attr("class", "topic-basewords")
+          .text(d => d[2].join(" "))
+          .style("text-align", "left")
 
       let svg = lines
         .append("td")
-        .attr("class", "freqblock")
-        .style("text-align", "right")
-        .style("width", "15vw")
+          .attr("class", "freqblock")
+          .style("text-align", "right")
+          .style("width", "15vw")
         .append("svg")
-        .attr("class", "freqblock")
-        .style("width", "15vw")
-        .style("height", "1em")
+          .attr("class", "freqblock")
+          .style("width", "15vw")
+          .style("height", "1em")
         .append("rect")
-        .style("text-align", "left")
-        .attr("height", "1em")
-        .attr("width", d => (15*d[1]/maxprop) + "vw")
-        .attr("x", d => (15 - 15*d[1]/maxprop) + "vw")
-        .attr("fill", "#aaa")
+          .style("text-align", "left")
+          .attr("height", "1em")
+          .attr("width", d => (15*d[1]/maxprop) + "vw")
+          .attr("x", d => (15 - 15*d[1]/maxprop) + "vw")
+          .attr("fill", "#aaa")
 
       lines
         .append("td")
-        .attr("class", "percentage")
-        .text((i) => percent(i[1]))
-        .style("width", "5vw")
-        .style("text-align", "left")
+          .attr("class", "topic-proportion")
+          .text(d => percent(d[1]))
+          .style("width", "5vw")
+          .style("text-align", "left")
+          .style("width")
+
     }
 
     function maketable() {
       let data  = [
-        {"text": "ID", "width": "4vw" },
-        {"text": "over time", "width": "11vw"},
-        {"text": "base words", "width": "60vw"},
-        {"text": "proportion", "width": "20vw"},
+        {"class": "topic-id", "text": "id", "width": "4vw" },
+        {"class": "topic-overtime", "text": "over time", "width": "11vw"},
+        {"class": "topic-basewords", "text": "base words", "width": "60vw"},
+        {"class": "topic-proportion", "text": "proportion", "width": "20vw"},
       ]
 
       let table = d3
-        .select("div.wrapper")
+        .select("div.topic-wrapper")
         .append("table")
-        .attr("class", "topic-content")
-        .selectAll("div")
+          .attr("class", "topic-content")
 
-      let toclass = (s) => "topic-top-row-" + s.split(" ").join("-")
-      let toelem  = (s) => `.${toclass(s)}`
-      let annotate = (text, asc) => `${text} ${asc?"\u25BC":"\u25B2"}`
-      let sorter = new Sorter
-      let header = table
+      table
+        .selectAll("tr") // doesn't matter here
         .append('tr')
-        .attr("class", "top-row")
         .data(data)
         .enter()
         .append("th")
-        .text(d => d["text"] === "ID" ? d["text"] + " \u25BC": d["text"])
-        .attr("class", d => toclass(d["text"]))
-        .attr("colspan", d => d["text"] === "proportion" ? "2" : "1")
-        .style("cursor", "pointer")
-        .style("width", d => d["width"])
-        .on("click", d => {
-          let[oldtext, ascend] = sorter.sort(d["text"]),
-              newtext= annotate(d["text"], ascend)
+          .attr("class", "sort topic-header")
+          .attr("data-sort", d => d.class)
+          .attr("colspan", d => d.text === "proportion" ? "2" : "1")
+          .style("width", d => d.width)
+          .text(d => d.text)
 
-          d3.select(toelem(oldtext))
-            .text(oldtext)
-          d3.select(toelem(d['text']))
-            .text(newtext)
-        })
+      let tbody = table
+        .append("tbody")
+          .attr("class", "topic-content list")
 
-      return table
+      return [tbody, data.map(d => d.class)]
     }
 
     function main(keys) {
-      let table = maketable(),
-          lines = makelines(table, keys)
+      let [tbody, value_names] = maketable(),
+          lines = makelines(tbody, keys)
+
+      new List('outer-topic-wrapper', {
+        valueNames: value_names
+      })
+
     }
 
     return main(keys)
   }
-
-
-
-  function update(keys) {
-
-    function makegraph(item) {
-      let svg = item
-        .select("td.overtime")
-        .select("svg.overtime")
-
-      let height = 3,
-          width  = 10,
-          colwidth = width / 104 // there are 104 years
-
-      svg
-        .selectAll("rect")
-        .data(d => d[3])
-        .attr("height", d => height*d + "em")
-        .attr("y", d => (height - height*d) + "em")
-
-    }
-
-    function updatelines(table, keys) {
-      let maxprob = d3.max(keys.map((i) => i[1])),
-          percent = (i) => Math.round(i*100000)/1000 + "%"
-
-      let lines = table
-        .selectAll("tr")
-        .data(keys)
-
-      lines
-        .select("td.id")
-        .text((i) => i[0])
-
-      makegraph(lines)
-
-      lines
-        .select("td.words")
-        .text((i) => i[2].join(" "))
-
-      let svg = lines
-        .select("td.freqblock")
-        .select("svg.freqblock")
-        .select("rect")
-        .attr("width", d => (15*d[1]/maxprob) + "vw")
-        .attr("x", d => (15 - 15*d[1]/maxprob) + "vw")
-
-      lines
-        .select("td.percentage")
-        .text((i) => percent(i[1]))
-
-    }
-
-    function main(keys) {
-      let table = d3
-        .select("table.topic-content")
-      let lines = updatelines(table, keys)
-    }
-
-    return main(keys)
-
-  }
-
-  class Sorter {
-
-    constructor() {
-      this.sort_last = "ID"
-      this.ascending = true
-    }
-
-
-    sort(name) {
-
-      function sorter(name, ascending) {
-        let means = data['means']
-        let comparison = ascending ? {
-          "ID":         (a, b) => a[0] - b[0],
-          "over time":  (a, b) => means[a[0]] - means[b[0]],
-          "base words": (a, b) => array_comp(a[2], b[2]),
-          "proportion": (a, b) => a[1] - b[1]
-        } : {
-          "ID":         (a, b) => - (a[0] - b[0]),
-          "over time":  (a, b) => - (means[a[0]] - means[b[0]]),
-          "base words": (a, b) => - array_comp(a[2], b[2]),
-          "proportion": (a, b) => - (a[1] - b[1])
-        }
-
-        return update(data['coll_keys'].sort(comparison[name]))
-      }
-
-      function main(object, name) {
-        let oldtext = object.sort_last
-        if (object.sort_last === name)
-          object.ascending = !object.ascending
-        else
-          object.sort_last = name,
-          object.ascending = false
-        sorter(object.sort_last, object.ascending)
-        return [oldtext, object.ascending]
-      }
-
-      return main(this, name)
-    }
-
-  }
-
-
 
   function callback(error, keys) {
-    data['coll_keys'] = keys
-    data['means'] = compute_mean(keys)
-    //data['coll_years'] = years
-
+    data.coll_keys = keys
+    compute_mean(keys)
     initialize(keys)
 
     d3.select("section.background")
       .style("transition", "1.5s")
       .style("opacity", "0")
 
-    d3.select("div.wrapper")
+    d3.select("div.topic-wrapper")
       .transition()
       .delay(500)
       .style("transition", "1.5s")
@@ -283,7 +165,7 @@
 
   d3.queue()
     .defer(d3.json, "https://eight1911.github.io/hansard/data/coll/keys.json")
-    //.defer(d3.json, "https://eight1911.github.io/page/data/coll/years.json")
+  //.defer(d3.json, "https://eight1911.github.io/hansard/data/coll/years.json")
     .await(callback)
 
 })()
--- js/index.js
+++ js/index.js
@@ -9,22 +9,9 @@ var data = {
 };
 "use strict";
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 (function () {
-
-  function array_comp(a, b) {
-
-    var min = Math.min(a.length, b.length);
-    for (var i = 0; i < min; ++i) {
-      if (a[i] > b[i]) return 1;else if (a[i] < b[i]) return -1;
-    }
-    return a.length - b.length;
-  }
 
   function compute_mean(keys) {
 
@@ -36,21 +23,21 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       }return summ / (array.length * norm);
     }
 
-    return keys.map(function (d) {
-      return expectation(d[3]);
+    return keys.forEach(function (d) {
+      return d.push(expectation(d[3]));
     });
   }
 
   function initialize(keys) {
 
     function makegraph(item) {
-      var td = item.append("td").attr("class", "overtime");
+      var td = item.append("td");
 
       var height = 3,
           width = 10,
           colwidth = width / 104,
           // there are 104 years
-      svg = td.append("svg").attr("class", "overtime").style("width", width + "vw").style("height", height + "em").style("text-align", "center").selectAll("rect").data(function (d) {
+      svg = td.append("svg").attr("class", "topic-graph").style("width", width + "vw").style("height", height + "em").style("text-align", "center").selectAll("rect").data(function (d) {
         return d[3];
       }).enter();
 
@@ -65,7 +52,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     }
 
     // add a topic line to a div with data already entered
-    function makelines(table, keys) {
+    function makelines(tbody, keys) {
       var maxprop = d3.max(keys.map(function (i) {
         return i[1];
       })),
@@ -73,17 +60,21 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         return Math.round(i * 100000) / 1000 + "%";
       };
 
-      var lines = table.data(keys).enter().append("tr");
+      var lines = tbody.selectAll("tr").data(keys).enter().append("tr");
 
-      lines.append("td").attr("class", "id").style("text-align", "center").text(function (i) {
-        return i[0];
+      lines.append("td").attr("class", "topic-id").style("text-align", "center").text(function (d) {
+        return d[0];
       });
 
       makegraph(lines);
+      // this is for sorting with list.js only
+      lines.append("td").attr("class", "topic-overtime").text(function (d) {
+        return percent(d[4]);
+      }).style("display", "none");
 
-      lines.append("td").attr("class", "words").style("text-align", "left").text(function (i) {
-        return i[2].join(" ");
-      });
+      lines.append("td").attr("class", "topic-basewords").text(function (d) {
+        return d[2].join(" ");
+      }).style("text-align", "left");
 
       var svg = lines.append("td").attr("class", "freqblock").style("text-align", "right").style("width", "15vw").append("svg").attr("class", "freqblock").style("width", "15vw").style("height", "1em").append("rect").style("text-align", "left").attr("height", "1em").attr("width", function (d) {
         return 15 * d[1] / maxprop + "vw";
@@ -91,185 +82,60 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         return 15 - 15 * d[1] / maxprop + "vw";
       }).attr("fill", "#aaa");
 
-      lines.append("td").attr("class", "percentage").text(function (i) {
-        return percent(i[1]);
-      }).style("width", "5vw").style("text-align", "left");
+      lines.append("td").attr("class", "topic-proportion").text(function (d) {
+        return percent(d[1]);
+      }).style("width", "5vw").style("text-align", "left").style("width");
     }
 
     function maketable() {
-      var data = [{ "text": "ID", "width": "4vw" }, { "text": "over time", "width": "11vw" }, { "text": "base words", "width": "60vw" }, { "text": "proportion", "width": "20vw" }];
+      var data = [{ "class": "topic-id", "text": "id", "width": "4vw" }, { "class": "topic-overtime", "text": "over time", "width": "11vw" }, { "class": "topic-basewords", "text": "base words", "width": "60vw" }, { "class": "topic-proportion", "text": "proportion", "width": "20vw" }];
 
-      var table = d3.select("div.wrapper").append("table").attr("class", "topic-content").selectAll("div");
+      var table = d3.select("div.topic-wrapper").append("table").attr("class", "topic-content");
 
-      var toclass = function toclass(s) {
-        return "topic-top-row-" + s.split(" ").join("-");
-      };
-      var toelem = function toelem(s) {
-        return "." + toclass(s);
-      };
-      var annotate = function annotate(text, asc) {
-        return text + " " + (asc ? "\u25BC" : "\u25B2");
-      };
-      var sorter = new Sorter();
-      var header = table.append('tr').attr("class", "top-row").data(data).enter().append("th").text(function (d) {
-        return d["text"] === "ID" ? d["text"] + " \u25BC" : d["text"];
-      }).attr("class", function (d) {
-        return toclass(d["text"]);
+      table.selectAll("tr") // doesn't matter here
+      .append('tr').data(data).enter().append("th").attr("class", "sort topic-header").attr("data-sort", function (d) {
+        return d.class;
       }).attr("colspan", function (d) {
-        return d["text"] === "proportion" ? "2" : "1";
-      }).style("cursor", "pointer").style("width", function (d) {
-        return d["width"];
-      }).on("click", function (d) {
-        var _sorter$sort = sorter.sort(d["text"]),
-            _sorter$sort2 = _slicedToArray(_sorter$sort, 2),
-            oldtext = _sorter$sort2[0],
-            ascend = _sorter$sort2[1],
-            newtext = annotate(d["text"], ascend);
-
-        d3.select(toelem(oldtext)).text(oldtext);
-        d3.select(toelem(d['text'])).text(newtext);
+        return d.text === "proportion" ? "2" : "1";
+      }).style("width", function (d) {
+        return d.width;
+      }).text(function (d) {
+        return d.text;
       });
 
-      return table;
+      var tbody = table.append("tbody").attr("class", "topic-content list");
+
+      return [tbody, data.map(function (d) {
+        return d.class;
+      })];
     }
 
     function main(keys) {
-      var table = maketable(),
-          lines = makelines(table, keys);
+      var _maketable = maketable(),
+          _maketable2 = _slicedToArray(_maketable, 2),
+          tbody = _maketable2[0],
+          value_names = _maketable2[1],
+          lines = makelines(tbody, keys);
+
+      new List('outer-topic-wrapper', {
+        valueNames: value_names
+      });
     }
 
     return main(keys);
   }
-
-  function update(keys) {
-
-    function makegraph(item) {
-      var svg = item.select("td.overtime").select("svg.overtime");
-
-      var height = 3,
-          width = 10,
-          colwidth = width / 104; // there are 104 years
-
-      svg.selectAll("rect").data(function (d) {
-        return d[3];
-      }).attr("height", function (d) {
-        return height * d + "em";
-      }).attr("y", function (d) {
-        return height - height * d + "em";
-      });
-    }
-
-    function updatelines(table, keys) {
-      var maxprob = d3.max(keys.map(function (i) {
-        return i[1];
-      })),
-          percent = function percent(i) {
-        return Math.round(i * 100000) / 1000 + "%";
-      };
-
-      var lines = table.selectAll("tr").data(keys);
-
-      lines.select("td.id").text(function (i) {
-        return i[0];
-      });
-
-      makegraph(lines);
-
-      lines.select("td.words").text(function (i) {
-        return i[2].join(" ");
-      });
-
-      var svg = lines.select("td.freqblock").select("svg.freqblock").select("rect").attr("width", function (d) {
-        return 15 * d[1] / maxprob + "vw";
-      }).attr("x", function (d) {
-        return 15 - 15 * d[1] / maxprob + "vw";
-      });
-
-      lines.select("td.percentage").text(function (i) {
-        return percent(i[1]);
-      });
-    }
-
-    function main(keys) {
-      var table = d3.select("table.topic-content");
-      var lines = updatelines(table, keys);
-    }
-
-    return main(keys);
-  }
-
-  var Sorter = function () {
-    function Sorter() {
-      _classCallCheck(this, Sorter);
-
-      this.sort_last = "ID";
-      this.ascending = true;
-    }
-
-    _createClass(Sorter, [{
-      key: "sort",
-      value: function sort(name) {
-
-        function sorter(name, ascending) {
-          var means = data['means'];
-          var comparison = ascending ? {
-            "ID": function ID(a, b) {
-              return a[0] - b[0];
-            },
-            "over time": function overTime(a, b) {
-              return means[a[0]] - means[b[0]];
-            },
-            "base words": function baseWords(a, b) {
-              return array_comp(a[2], b[2]);
-            },
-            "proportion": function proportion(a, b) {
-              return a[1] - b[1];
-            }
-          } : {
-            "ID": function ID(a, b) {
-              return -(a[0] - b[0]);
-            },
-            "over time": function overTime(a, b) {
-              return -(means[a[0]] - means[b[0]]);
-            },
-            "base words": function baseWords(a, b) {
-              return -array_comp(a[2], b[2]);
-            },
-            "proportion": function proportion(a, b) {
-              return -(a[1] - b[1]);
-            }
-          };
-
-          return update(data['coll_keys'].sort(comparison[name]));
-        }
-
-        function main(object, name) {
-          var oldtext = object.sort_last;
-          if (object.sort_last === name) object.ascending = !object.ascending;else object.sort_last = name, object.ascending = false;
-          sorter(object.sort_last, object.ascending);
-          return [oldtext, object.ascending];
-        }
-
-        return main(this, name);
-      }
-    }]);
-
-    return Sorter;
-  }();
 
   function callback(error, keys) {
-    data['coll_keys'] = keys;
-    data['means'] = compute_mean(keys);
-    //data['coll_years'] = years
-
+    data.coll_keys = keys;
+    compute_mean(keys);
     initialize(keys);
 
     d3.select("section.background").style("transition", "1.5s").style("opacity", "0");
 
-    d3.select("div.wrapper").transition().delay(500).style("transition", "1.5s").style("opacity", "1");
+    d3.select("div.topic-wrapper").transition().delay(500).style("transition", "1.5s").style("opacity", "1");
   }
 
   d3.queue().defer(d3.json, "https://eight1911.github.io/hansard/data/coll/keys.json")
-  //.defer(d3.json, "https://eight1911.github.io/page/data/coll/years.json")
+  //.defer(d3.json, "https://eight1911.github.io/hansard/data/coll/years.json")
   .await(callback);
 })();
